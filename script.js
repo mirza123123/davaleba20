1d58b2efc327a63439cb3a138cceb06a77e32ea9
mirza123123 <mirzakapanadze55@gmail.com>
2024-11-27 14:09:58 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// // რიცხვები 1დან 5მდე
+// x = 1
+// while( x <= 5){
+//     console.log(x);
+//     x++;
+// }
+
+
+
+
+
+// პაროლის შემოწმება
+// const correctPassword = "1234";
+// let inputPassword;
+
+// while (true) {
+//     inputPassword = prompt("ჩაწერეთ პაროლი:");
+//     if (inputPassword === correctPassword) {
+//         console.log("პაროლი სწორია.");
+//         break;
+//     } else {
+//         console.log("პაროლი არასწორია.");
+//     }
+// }
+
+
+
+
+// მასივის კონსოლზე გამოტანა
+// const xili = ["vasli","msxali","atami"];
+// for (let i = 0; i < xili.length; i++)  {
+//     console.log(xili[i])
+// }
+
+
+
+
+
+
+// // რიცხვების დაჰჯამება 1დან 10მდე
+// let sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//     sum += i;
+// }
+
+// console.log("The sum is: " + sum);
